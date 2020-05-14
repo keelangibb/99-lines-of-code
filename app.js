836bf3b580c956ab891775ce0c525ab1fa24984f
keelangibb <keelangibb@gmail.com>
2020-05-14 15:25:29 -0700

    got rid of stuff that wasn't needed and simplified

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 let friends = ['Brandon', 'Anna', 'Timothy', 'Shilah', 'Brittany'];
-let x = 100
-let y = x - 1
-let z = 0
-let names = friends[z]
-lyric1 = 'lines';
-lyric2 = 'line';
-
-for (i = 0; i < friends.length; i++) {
-    x = 100
-    y = x - 1
-
-    for (j = 0; j < 97; j++) {
-        x--
-        y--
-        let lyrics = x + ' lines of code in the file, ' + x + ' lines of code; ' + friends[z] + ' strikes one out, clears it all out, ' + y + ' lines of code in the file';
-        console.log(lyrics)
+for (let i = 0; i < friends.length; i++) {
+    for (let j = 99; j > 0; j--) {
+        if (j >= 3) {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + [j - 1] + ' lines of code in the file');
+        }
+        else if (j >= 2) {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + [j - 1] + ' more line of code in the file');
+        }
+        else {
+            console.log(j + ' line of code in the file, ' + j + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, no more lines of code in the file');
+        }
     }
-    x--
-    y--
-    let lyrics2 = x + ' line of code in the file, ' + x + ' lines of code; ' + friends[z] + ' strikes one out, clears it all out, ' + y + ' more line of code in the file';
-    console.log(lyrics2);
-    lyrics2 = 1 + ' line of code in the file, ' + 1 + ' line of code; ' + friends[z] + ' strikes one out, clears it all out, no more line of code in the file';
-    console.log(lyrics2)
-    z++
 }
